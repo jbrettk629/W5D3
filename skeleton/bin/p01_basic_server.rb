c6e0ab9d51d388346281b8d228d24588024d8978
jbrettk629 <jbrettk629@gmail.com>
2018-09-19 15:05:07 -0700

    Complete Part I Phase I

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,26 @@
 require 'rack'
+
+# Rack::Server.start(
+#   app: Proc.new do |env|
+#     ["200", {"Content-Type"=>"text/html"}, ["Hello Word"]]
+#   end 
+# )
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new 
+  res["Content-Type"] = "text/html"
+  # if rec.path == 
+  #   path = /w*
+  #   res.write(path)
+  # else 
+  #   res.write("Hello World!")
+  # end
+  res.write(req.path) 
+  res.finish 
+end 
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)

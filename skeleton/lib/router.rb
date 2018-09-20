b445267337490c3430fe1605dd27bb3fcfefc417
jbrettk629 <jbrettk629@gmail.com>
2018-09-19 17:33:33 -0700

    Screw around with Route in Part I Phase V

--- skeleton/lib/router.rb
+++ skeleton/lib/router.rb
@@ -2,15 +2,22 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    req.request_method.downcase.to_sym == @http_method && req.path =~ @pattern
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    controller = @controller_class.new(req, res)
+    controller.call(@action_name)
   end
 end
 
@@ -32,6 +39,7 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    
   end
 
   # should return the route that matches this request
